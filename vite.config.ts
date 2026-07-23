2ed2bdc3b8cb28d8aa48201df1beab4e06f56fc6
Ec-Dev <erinzon_1995@live.com>
2026-07-22 23:13:02 -0500

    Configure basepath in TanStack Router for subdirectory hosting

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-  base:"/cumpleanos",
+  base:"https://ejcc95.github.io/cumpleanos/",
   plugins: [
     TanStackRouterVite(),
     tailwindcss(),
