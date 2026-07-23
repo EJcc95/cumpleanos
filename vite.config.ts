d91c1b04a0226dd65b37cd7d6d908f0e79262e3d
Ec-Dev <erinzon_1995@live.com>
2026-07-22 22:50:05 -0500

    prueba deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 export default defineConfig({
-  base: '/cumpleanos/',
   plugins: [
     TanStackRouterVite(),
     tailwindcss(),
